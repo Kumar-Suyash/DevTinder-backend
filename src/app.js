7423461233867a5e5fb13fce966b43b3cc479b0b
kumar Suyash <ksuyash001@gmail.com>
2025-12-25 16:28:41 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
 
-
-app.use("/",(req, res)=>{
-    res.send("Hello from the dashboard!");
-});
-
 app.use("/test",(req, res) =>{
     res.send("Hello from the server");
 });
 
-app.use("/home",(req,res)=>{
-    res.send("Hello Hello Hello from the home route");
+//! All the API call are handled over here and the orders also matter
+
+//! And all the methods are matched over here
+// app.use("/user",(req, res) =>{
+//     res.send("HEHEHHE ......");
+// });
+
+
+//* This will handle only GET call from /user
+
+app.get("/user" , (req , res ) => {
+    res.send({firstname : "Kumar" , lastname : "Suyash"})
+});
+
+//* This will handle only POST call from /user
+
+app.post("/user" , (req , res) =>{
+    //! logic to save data in DB...
+    res.send("Data is sucessfully saved in db")
+});
+
+//* This will handle only DELETE call from /user
+
+app.delete("/user", (req, res) =>{
+    res.send("Deleted successfully")
 });
 
 app.listen(7777,(req, res)=>{
--- src/readme.md
+++ src/readme.md
@@ -14,3 +14,10 @@
 
 -Install nodemon and update scripts inside package.json
 
+- Play with routes and rotes extensions ex. "/" , "/home", "/home/2", "xyz"
+
+- Order of the rotes matter a lot "/" --> better Understanding for this route.
+
+-Install Postman App create Workspace/Collection > test an API call.
+
+- Write logics to handle all the API call for GET, POST, PUT, PATCH, DELETE and test them on postman.
